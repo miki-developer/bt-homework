ad1b7836e12635888b6f75999be40875e148286c
miki-developer <73535222+miki-developer@users.noreply.github.com>
2022-04-28 10:49:15 +0200

    Create hw10.php

---
+++ hw10.php
@@ -0,0 +1,24 @@
+<?php
+//1.
+$a = 200;
+$b = 80;
+$add = $a + $b;
+$sub = $a - $b;
+$multi = $a * $b;
+$div = $a / $b;
+echo " addition of 200 and 80 is: $add <br>\n subtraction is: $sub <br>\n  multiplication is: $multi <br>\n and division is: $div <br>\n";
+//2.
+$day = 5;
+if($day == 5){
+    echo "danas je petak <br>\n";
+}else {
+  echo  "danas nije petak <br>\n";
+}
+//3.
+
+$num1 = 3;
+$num2 = 6;
+$num3 = 9;
+$sum = $num1 + $num2 + $num3;
+echo "zbir brojeva $num1, $num2, $num3 je $sum;"
+?>
